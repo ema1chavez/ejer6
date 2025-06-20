3215b7380f6600c484d9ccfc9ac803522471feb4
Ema <60031750@idexperujapon.edu.pe>
2025-06-20 16:22:11 -0500

    Agregar archivos de proyecto.

---
+++ ejer6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1;
+            Console.WriteLine("Introduce un número entero: ");
+            n1=Convert.ToInt32(Console.ReadLine());
+            if (n1>0);
+            {
+                Console.WriteLine("El número es positivo");
+            }
+
+            }
+    }
+}
